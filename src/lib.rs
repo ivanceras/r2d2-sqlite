8ea9f422493bb2fb7b4eea71e6a82a35d2dfca9c
jcesar <jovansonlee.cesar@truste.com>
2015-07-22 16:38:12 +0800

    Based on r2d2-postgres for sqlite

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+//! Sqlite support for the `r2d2` connection pool.
+extern crate r2d2;
+extern crate rusqlite;
+
+use std::error;
+use std::error::Error as _StdError;
+use std::fmt;
+
+use rusqlite::SqliteError;
+use rusqlite::SqliteConnection;
+
+use std::path::Path;
+
+/// A unified enum of errors returned by postgres::Connection
+#[derive(Debug)]
+pub enum Error {
+    Connect(SqliteError),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{}: {}", self.description(), self.cause().unwrap())
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::Connect(_) => "Error opening a connection",
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            Error::Connect(ref err) => Some(err as &error::Error),
+        }
+    }
+}
+
+
+pub struct SqliteConnectionManager {
+    in_memory: bool,
+    path: Option<String>,
+}
+
+impl SqliteConnectionManager {
+    
+    pub fn new(database: &str)-> Result<SqliteConnectionManager, SqliteError>{
+        let in_memory = match database{
+            ":memory:" => true,
+            _ => false,};
+        let path = if !in_memory{
+            Some(database.to_owned())
+        }else{None};
+        Ok(SqliteConnectionManager {in_memory: in_memory, path: path})
+    }
+}
+
+impl r2d2::ManageConnection for SqliteConnectionManager {
+    type Connection = SqliteConnection;
+    type Error = Error;
+
+    fn connect(&self) -> Result<SqliteConnection, Error> {
+        if self.in_memory{
+            Ok(SqliteConnection::open_in_memory().unwrap())
+        }
+        else if self.path.is_some(){
+            Ok(SqliteConnection::open(&Path::new(self.path.as_ref().unwrap())).unwrap())
+        }
+        else{
+            unreachable!()
+        }
+    }
+
+    fn is_valid(&self, conn: &mut SqliteConnection) -> Result<(), Error> {
+        conn.execute_batch("").map_err(Error::Connect)
+    }
+
+    fn has_broken(&self, conn: &mut SqliteConnection) -> bool {
+        false
+    }
+}
---
+++ tests/test.rs
@@ -0,0 +1,80 @@
+extern crate rusqlite;
+extern crate r2d2;
+extern crate r2d2_sqlite;
+
+use std::sync::Arc;
+use std::sync::mpsc;
+use std::thread;
+
+use r2d2_sqlite::SqliteConnectionManager;
+
+#[test]
+fn test_basic() {
+    let manager = SqliteConnectionManager::new(":memory:").unwrap();
+    let config = r2d2::Config::builder().pool_size(2).build();
+    let pool = Arc::new(r2d2::Pool::new(config, manager).unwrap());
+
+    let (s1, r1) = mpsc::channel();
+    let (s2, r2) = mpsc::channel();
+
+    let pool1 = pool.clone();
+    let t1 = thread::spawn(move || {
+        let conn = pool1.get().unwrap();
+        s1.send(()).unwrap();
+        r2.recv().unwrap();
+        drop(conn);
+    });
+
+    let pool2 = pool.clone();
+    let t2 = thread::spawn(move || {
+        let conn = pool2.get().unwrap();
+        s2.send(()).unwrap();
+        r1.recv().unwrap();
+        drop(conn);
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    pool.get().unwrap();
+}
+
+#[test]
+fn test_file() {
+    let manager = SqliteConnectionManager::new("file.db").unwrap();
+    let config = r2d2::Config::builder().pool_size(2).build();
+    let pool = Arc::new(r2d2::Pool::new(config, manager).unwrap());
+
+    let (s1, r1) = mpsc::channel();
+    let (s2, r2) = mpsc::channel();
+
+    let pool1 = pool.clone();
+    let t1 = thread::spawn(move || {
+        let conn = pool1.get().unwrap();
+        s1.send(()).unwrap();
+        r2.recv().unwrap();
+        drop(conn);
+    });
+
+    let pool2 = pool.clone();
+    let t2 = thread::spawn(move || {
+        let conn = pool2.get().unwrap();
+        s2.send(()).unwrap();
+        r1.recv().unwrap();
+        drop(conn);
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    pool.get().unwrap();
+}
+
+#[test]
+fn test_is_valid() {
+    let manager = SqliteConnectionManager::new(":memory:").unwrap();
+    let config = r2d2::Config::builder().pool_size(1).test_on_check_out(true).build();
+    let pool = r2d2::Pool::new(config, manager).unwrap();
+
+    pool.get().unwrap();
+}
