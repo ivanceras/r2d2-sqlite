2d58dd0805ca42befb336e1c3c4e1a058421e37c
Jovansonlee Cesar <ivanceras@gmail.com>
2016-06-10 16:41:28 +0800

    add username to update-docs

--- update-docs.sh
+++ update-docs.sh
@@ -3,6 +3,8 @@ cargo clean
 cargo doc --no-deps
 cd target/doc
 git init
+git config user.name "Jovansonlee Cesar"
+git config user.email "ivanceras@gmail.com"
 git add . -A
 git commit -m "Commiting docs to github pages"
 git remote add origin https://github.com/ivanceras/r2d2-sqlite
