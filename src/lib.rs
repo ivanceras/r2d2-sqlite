9b9030bc1635be43cbf8d17b0ea5220c11da1e8a
Dippi <dippi@altervista.org>
2016-06-10 03:36:39 +0200

    Update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,34 @@
-//! Sqlite support for the `r2d2` connection pool.
+//! # Sqlite support for the `r2d2` connection pool.
+//!
+//! Library crate: [r2d2-sqlite](https://crates.io/crates/r2d2-sqlite/)
+//!
+//! Integrated with: [r2d2](https://crates.io/crates/r2d2)
+//! and [rusqlite](https://crates.io/crates/rusqlite)
+//!
+//! ## Example
+//!
+//! ```rust,no_run
+//! extern crate r2d2;
+//! extern crate r2d2_sqlite;
+//! extern crate rusqlite;
+//!
+//! use std::thread;
+//! use r2d2_sqlite::SqliteConnectionManager;
+//!
+//! fn main() {
+//!     let config = r2d2::Config::default();
+//!     let manager = SqliteConnectionManager::new_in_memory();
+//!     let pool = r2d2::Pool::new(config, manager).unwrap();
+//!
+//!     for i in 0..10i32 {
+//!         let pool = pool.clone();
+//!         thread::spawn(move || {
+//!             let conn = pool.get().unwrap();
+//!             conn.execute("INSERT INTO foo (bar) VALUES ($1)", &[&i]).unwrap();
+//!         });
+//!     }
+//! }
+//! ```
 extern crate r2d2;
 extern crate rusqlite;
 
@@ -8,6 +38,7 @@ use std::convert;
 
 use rusqlite::{Connection, Error as RusqliteError, OpenFlags};
 
+/// A unified enum of errors returned by `rusqlite::Connection`
 #[derive(Debug)]
 pub enum Error {
     Connect(RusqliteError),
@@ -45,23 +76,36 @@ enum ConnectionConfig {
     File(String, OpenFlags),
 }
 
+/// An `r2d2::ManageConnection` for `rusqlite::Connection`s.
 pub struct SqliteConnectionManager {
     config: ConnectionConfig,
 }
 
 impl SqliteConnectionManager {
+    /// Creates a new `SqliteConnectionManager` from file.
+    ///
+    /// See `rusqlite::Connection::open`
     pub fn new(database: &str) -> Self {
         Self::new_with_flags(database, OpenFlags::default())
     }
 
+    /// Creates a new `SqliteConnectionManager` from file with open flags.
+    ///
+    /// See `rusqlite::Connection::open_with_flags`
     pub fn new_with_flags(database: &str, flags: OpenFlags) -> Self {
         SqliteConnectionManager { config: ConnectionConfig::File(database.to_string(), flags) }
     }
 
+    /// Creates a new `SqliteConnectionManager` in memory.
+    ///
+    /// See `rusqlite::Connection::open_in_memory`
     pub fn new_in_memory() -> Self {
         Self::new_in_memory_with_flags(OpenFlags::default())
     }
 
+    /// Creates a new `SqliteConnectionManager` in memory with open flags.
+    ///
+    /// See `rusqlite::Connection::open_in_memory_with_flags`
     pub fn new_in_memory_with_flags(flags: OpenFlags) -> Self {
         SqliteConnectionManager { config: ConnectionConfig::InMemory(flags) }
     }
