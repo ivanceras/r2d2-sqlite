375bf831c983fd459728bee1d3df46ede9053cec
shangchenglumetro <shuang.cui@live.com>
2025-07-04 16:41:25 +0800

    chore: fix typo in update-docs.sh
    Signed-off-by: shangchenglumetro <shuang.cui@live.com>

--- update-docs.sh
+++ update-docs.sh
@@ -6,7 +6,7 @@ git init
 git config user.name "Jovansonlee Cesar"
 git config user.email "ivanceras@gmail.com"
 git add . -A
-git commit -m "Commiting docs to github pages"
+git commit -m "Committing docs to github pages"
 git remote add origin https://github.com/ivanceras/r2d2-sqlite
 git checkout -b gh-pages
 git push --force origin gh-pages
